328018d8716dcee5295822e2d8b673d123c965b0
season-123-docom <bobo10161995@gmail.com>
2024-11-20 16:07:57 +0800

    1120uploadweb

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
+  //倉庫路徑(手寫)
+ base:process.env.NODE_ENV ==='production' ? '/demoProject/':'/',
   plugins: [react()],
 })
